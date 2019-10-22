a1657698f9f4ba612f7086410724640c9d9cccd5
Andy Jennings <andyj@shazbot.net>
2019-10-22 09:26:27 -0400

    Initial Commit

---
+++ src/nyc/codeup/Main.java
@@ -0,0 +1,21 @@
+package nyc.codeup;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+	    Main.sequenceOfNumbers();
+    }
+
+    public static void test()
+    {
+        System.out.println("This is a TEST!");
+    }
+
+    public static void sequenceOfNumbers()
+    {
+        for ( ;  ; ) {
+
+        }
+    }
+}
